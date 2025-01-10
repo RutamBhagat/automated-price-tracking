8cfc75f49d64fe05f780e8dfbca1ebf7b72d4733
RutamBhagat <rutambhagat@gmail.com>
2025-01-10 17:37:12 +0530

    build(backend): add database integration and update environment configuration

--- backend/src/ui.py
+++ backend/src/ui.py
@@ -1,5 +1,14 @@
+import os
 import streamlit as st
+
 from utils import is_valid_url
+from database import Database
+from dotenv import load_dotenv
+
+load_dotenv()
+
+with st.spinner("Loading database..."):
+    db = Database(os.getenv("DATABASE_URL"))
 
 with st.sidebar:
     st.title("Add New Product")
@@ -12,6 +21,7 @@ with st.sidebar:
         elif not is_valid_url(product_url):
             st.error("Please enter a valid URL")
         else:
+            db.add_product(product_url)
             st.success("Product is now being tracked!")
 
 st.title("Price Tracker Dashboard")
