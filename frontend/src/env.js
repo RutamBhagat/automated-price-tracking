0666f6ba7e6046c29863a4cd03823caae22c1306
RutamBhagat <rutambhagat@gmail.com>
2025-01-11 23:23:27 +0530

    feat(auth): integrate NextAuth with Discord provider and Prisma adapter feat(database): add Prisma schema and database connection setup feat(env): update environment variables for authentication and database feat(scripts): add script to start local development database with Docker

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -7,7 +7,19 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    NODE_ENV: z.enum(["development", "test", "production"]),
+    AUTH_SECRET:
+      process.env.NODE_ENV === "production"
+        ? z.string()
+        : z.string().optional(),
+    AUTH_DISCORD_ID: z.string(),
+    AUTH_DISCORD_SECRET: z.string(),
+    DATABASE_URL: z.string().url(),
+    FIRECRAWL_API_KEY: z.string(),
+    EMAIL_ADDRESS: z.string().email(),
+    EMAIL_APP_PASSWORD: z.string(),
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
   },
 
   /**
@@ -24,8 +36,14 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
+    AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+    FIRECRAWL_API_KEY: process.env.FIRECRAWL_API_KEY,
+    EMAIL_ADDRESS: process.env.EMAIL_ADDRESS,
+    EMAIL_APP_PASSWORD: process.env.EMAIL_APP_PASSWORD,
     NODE_ENV: process.env.NODE_ENV,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
