5b0ee79152539b1a3fad962e220a47a7d0c2521b
RutamBhagat <rutambhagat@gmail.com>
2025-01-12 16:49:12 +0530

    feat(frontend): replace HeroSection with Hero component and add LampDemo component for enhanced UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,14 +3,12 @@ import DemoSection from "@/components/landing-page/demo-section";
 import FAQSection from "@/components/landing-page/faq-section";
 import FeaturesSection from "@/components/landing-page/features-section";
 import Footer from "@/components/landing-page/footer";
-import HeroSection from "@/components/landing-page/hero-section";
 import { Hero } from "@/components/landing-page/hero-with-group-of-images-text-and-two-buttons";
 import HowItWorksSection from "@/components/landing-page/how-it-works";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <HeroSection />
       <Hero/>
       <FeaturesSection />
       <DemoSection />
