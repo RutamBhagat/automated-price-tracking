0266f1e4cb76dc2131d3db2470e43a3c2b01815e
RutamBhagat <rutambhagat@gmail.com>
2025-01-12 18:20:17 +0530

    feat(frontend): remove HeroWithLamp component and integrate its functionality into HeroWithImages for improved layout and animation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,18 +4,16 @@ import FAQSection from "@/components/landing-page/faq-section";
 import FeaturesSection from "@/components/landing-page/features-section";
 import Footer from "@/components/landing-page/footer";
 import { HeroWithImages } from "@/components/landing-page/hero-with-images";
-import { HeroWithLamp } from "@/components/landing-page/hero-with-lamp";
 import HowItWorksSection from "@/components/landing-page/how-it-works";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <HeroWithLamp/>
       <HeroWithImages/>
+      <AnimatedGradientStats />
       <FeaturesSection />
       <DemoSection />
       <HowItWorksSection />
-      <AnimatedGradientStats />
       <FAQSection />
       <Footer />
     </main>
--- frontend/src/components/landing-page/hero-with-images.tsx
+++ frontend/src/components/landing-page/hero-with-images.tsx
@@ -1,55 +1,108 @@
+"use client";
 import { MoveRight, PhoneCall } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
+import { motion } from "framer-motion";
+import { LampContainer } from "../ui/lamp";
 
 function HeroWithImages() {
   return (
-    <div className="w-full bg-slate-950">
-      <div className="container mx-auto px-4 py-20 lg:py-40">
-        <div className="grid grid-cols-1 items-center gap-8 md:grid-cols-2">
-          <div className="flex flex-col gap-4">
-            <div>
-              <Badge
-                variant="outline"
-                className="border-[#0e8ba0] text-[#0e8ba0]"
-              >
-                Now Live!
-              </Badge>
-            </div>
-            <h1 className="font-regular max-w-lg text-left text-5xl tracking-tighter text-gray-400 md:text-7xl">
-              PriceTracker
-            </h1>
+    <>
+      <LampContainer className="min-h-[41dvh]">
+        <></>
+      </LampContainer>
+      <div className="w-full bg-slate-950">
+        <div className="container mx-auto -translate-y-20 px-4 lg:pb-20">
+          <div className="grid grid-cols-1 items-center gap-8 md:grid-cols-2">
             <div className="flex flex-col gap-4">
-              <p className="max-w-md text-left text-xl leading-relaxed tracking-tight text-gray-400">
-                Save time and money by staying on top of price drops and stock
-                updates. Our platform delivers instant alerts, helping you make
-                smarter purchasing decisions, all in one seamless experience.
-              </p>
-            </div>
-            <div className="flex flex-row gap-4">
-              <Button
-                size="lg"
-                className="gap-4 border-[#0e8ba0] text-[#0e8ba0] hover:bg-[#0e8ba0] hover:text-white"
-                variant="outline"
+              <motion.div
+                initial={{ opacity: 0.5, y: 100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                  delay: 0.3,
+                  duration: 0.8,
+                  ease: "easeInOut",
+                }}
               >
-                Learn More <PhoneCall className="h-4 w-4" />
-              </Button>
-              <Button
-                size="lg"
-                className="gap-4 bg-[#16c5e2] text-[#020617] hover:bg-[#0e8ba0] hover:text-white"
+                <Badge
+                  variant="outline"
+                  className="border-[#0e8ba0] text-[#0e8ba0]"
+                >
+                  Now Live!
+                </Badge>
+              </motion.div>
+              <motion.h1
+                initial={{ opacity: 0.5, y: 100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                  delay: 0.3,
+                  duration: 0.8,
+                  ease: "easeInOut",
+                }}
+                className="font-regular max-w-lg text-left text-5xl tracking-tighter text-gray-400 md:text-7xl"
               >
-                Get Started Now <MoveRight className="h-4 w-4" />
-              </Button>
+                PriceTracker
+              </motion.h1>
+              <div className="flex flex-col gap-4">
+                <motion.p
+                  initial={{ opacity: 0.5, y: 100 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{
+                    delay: 0.3,
+                    duration: 0.8,
+                    ease: "easeInOut",
+                  }}
+                  className="max-w-md text-left text-xl leading-relaxed tracking-tight text-gray-400"
+                >
+                  Save time and money by staying on top of price drops and stock
+                  updates. Our platform delivers instant alerts, helping you
+                  make smarter purchasing decisions, all in one seamless
+                  experience.
+                </motion.p>
+              </div>
+              <motion.div
+                initial={{ opacity: 0.5, y: 100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                  delay: 0.3,
+                  duration: 0.8,
+                  ease: "easeInOut",
+                }}
+                className="flex flex-row gap-4"
+              >
+                <Button
+                  size="lg"
+                  className="gap-4 border-[#0e8ba0] text-[#0e8ba0] hover:bg-[#0e8ba0] hover:text-white"
+                  variant="outline"
+                >
+                  Learn More <PhoneCall className="h-4 w-4" />
+                </Button>
+                <Button
+                  size="lg"
+                  className="gap-4 bg-[#16c5e2] text-[#020617] hover:bg-[#0e8ba0] hover:text-white"
+                >
+                  Get Started Now <MoveRight className="h-4 w-4" />
+                </Button>
+              </motion.div>
             </div>
-          </div>
-          <div className="grid grid-cols-2 gap-8">
-            <div className="aspect-square rounded-md bg-gray-900"></div>
-            <div className="row-span-2 rounded-md bg-gray-900"></div>
-            <div className="aspect-square rounded-md bg-gray-900"></div>
+            <motion.div
+              initial={{ opacity: 0.5, y: 100 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{
+                delay: 0.3,
+                duration: 0.8,
+                ease: "easeInOut",
+              }}
+              className="grid grid-cols-2 gap-8"
+            >
+              <div className="aspect-square rounded-md bg-gray-900"></div>
+              <div className="row-span-2 rounded-md bg-gray-900"></div>
+              <div className="aspect-square rounded-md bg-gray-900"></div>
+            </motion.div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
